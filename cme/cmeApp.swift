6ea32d933e86756618807bf527ace9a99fb5e62c
Attia Elsayed <atiamohamedein@gmail.com>
2025-08-04 09:46:42 +0200

    Initial Commit

---
+++ cme/cmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cmeApp.swift
+//  cme
+//
+//  Created by Attia Elsayed on 04.08.25.
+//
+
+import SwiftUI
+
+@main
+struct cmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
